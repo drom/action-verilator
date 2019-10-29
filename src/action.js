a44654945d156ac804f6ca16bcbadc26d1bbab69
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2019-10-29 16:21:57 -0700

    experiments

--- src/action.js
+++ src/action.js
@@ -5,16 +5,17 @@ const core = require('@actions/core');
 console.log('action-sim:started');
 
 const myInput = core.getInput('version');
-try {
-  core.debug('Inside try block');
 
-  if (!myInput) {
-    core.warning('myInput was not set');
-  }
-}
-catch (err) {
-  core.error(`Error ${err}, action may still succeed though`);
-}
+// try {
+//   core.debug('Inside try block');
+//
+//   if (!myInput) {
+//     core.warning('myInput was not set');
+//   }
+// }
+// catch (err) {
+//   core.error(`Error ${err}, action may still succeed though`);
+// }
 
 console.log('action-sim:', myInput);
 
