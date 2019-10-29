399ed4736ffa754ac8e82acd9db220adeea73639
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2019-10-28 19:23:54 -0700

    forgot to browserify

--- dist/action.js
+++ dist/action.js
@@ -815,7 +815,17 @@ const core = require('@actions/core');
 
 console.log('action-sim:started');
 
-const myInput = core.getInput('name', { required: true });
+const myInput = core.getInput('inputName');
+try {
+  core.debug('Inside try block');
+
+  if (!myInput) {
+    core.warning('myInput was not set');
+  }
+}
+catch (err) {
+  core.error(`Error ${err}, action may still succeed though`);
+}
 
 console.log('action-sim:', myInput);
 
